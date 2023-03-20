1b5d5183f463085fb2de7325c9a4bc15fc7123f6
oh in seong <superohinsung@naver.com>
2023-03-20 17:35:44 +0900

    프래그먼트 수정

--- presentation/src/main/java/com/example/android_bong/view/home/ui/notifications/NotificationsFragment.kt
+++ presentation/src/main/java/com/example/android_bong/view/home/ui/notifications/NotificationsFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.android_bong.view.home.databinding.FragmentNotificationsBinding
+import com.example.android_bong.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
