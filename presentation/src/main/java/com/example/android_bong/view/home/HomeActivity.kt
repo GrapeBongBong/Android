d8fe0c7582d657bb74ddb42fa61853738c2815f1
oh in seong <superohinsung@naver.com>
2023-03-20 17:32:26 +0900

    HomeActivity 바텀 네비게이션 뷰 연결

--- presentation/src/main/java/com/example/android_bong/view/home/HomeActivity.kt
+++ presentation/src/main/java/com/example/android_bong/view/home/HomeActivity.kt
@@ -4,13 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.activity.viewModels
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android_bong.R
 import com.example.android_bong.common.ViewBindingActivity
 import com.example.android_bong.databinding.ActivityHomeBinding
 
 class HomeActivity : ViewBindingActivity<ActivityHomeBinding>() {
 
-    private val viewModel : HomeViewModel by viewModels()
+    override val bindingInflater: (LayoutInflater) -> ActivityHomeBinding
+        get() = ActivityHomeBinding::inflate
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     companion object {
         fun getIntent(context: Context): Intent {
@@ -18,10 +26,25 @@ class HomeActivity : ViewBindingActivity<ActivityHomeBinding>() {
         }
     }
 
-    override val bindingInflater: (LayoutInflater) -> ActivityHomeBinding
-        get() = ActivityHomeBinding::inflate
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         super.onCreate(savedInstanceState)
+
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_home)
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
