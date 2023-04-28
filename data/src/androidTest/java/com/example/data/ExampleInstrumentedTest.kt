22d837218483a02bc31c034f8116f35a8318d88e
oh in seong <superohinsung@naver.com>
2023-04-28 16:52:25 +0900

    data 모듈 이름 수정

--- data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.mylibrary
+package com.example.data
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- data/src/test/java/com/example/data/ExampleUnitTest.kt
+++ data/src/test/java/com/example/data/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.mylibrary
+package com.example.data
 
 import org.junit.Test
 
