048d1cb7d803ed9a5dec8884a8ea6b5e3b855136
oh in seong <superohinsung@naver.com>
2023-05-16 16:31:53 +0900

    회원가입 타이틀 설정

--- presentation/src/main/java/com/example/android_bong/view/main/MainActivity.kt
+++ presentation/src/main/java/com/example/android_bong/view/main/MainActivity.kt
@@ -43,7 +43,6 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
         }
     }
 
-    
     private fun initBottomNavigationView() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_home) as NavHostFragment
