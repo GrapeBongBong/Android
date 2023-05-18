02a8a75bf89dd97f1f8c6f0956641caae10672fd
oh in seong <superohinsung@naver.com>
2023-05-18 20:26:28 +0900

    comment Ui post Ui 완성

---
+++ presentation/src/main/java/com/example/android_bong/view/postdetail/PostDetailUiState.kt
@@ -0,0 +1,5 @@
+package com.example.android_bong.view.postdetail
+
+data class PostDetailUiState(
+    val userMessage: String? = null
+)
---
+++ presentation/src/main/java/com/example/android_bong/view/postdetail/PostDetailViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.android_bong.view.postdetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class PostDetailViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(PostDetailUiState())
+    val uiState = _uiState.asStateFlow()
+
+}
